384b0a20723113b6378cf96721ebb06a9a9b5806
910527ksj <910527ksj@gmail.com>
2018-10-18 17:54:40 +0900

    캐릭터 움직임
    캐릭터 움직임 수정

--- PangPang/Assets/Script/PlayerScript.cs
+++ PangPang/Assets/Script/PlayerScript.cs
@@ -6,37 +6,87 @@ public class PlayerScript : MonoBehaviour {
 
     public float playerSpeed;
     public Animator playerAnim;
+    public GameObject firePos;
+    public GameObject bullet;
+    public float shotDelay;
+    public float shotCool;
+    public float limtX;
 
-    void Start ()
+    void Start()
     {
-		
-	}
-	
+        shotDelay = 0;
+    }
 
-	
-	void Update ()
+
+
+    void Update ()
     {
-		if(Input.GetKey(KeyCode.LeftArrow))
+        shotDelay += Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            playerAnim.SetBool("Left Bool",true);
-            playerAnim.SetFloat("Left Float", 1);
+            GetComponent<Animator>().Play("left");
             transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
-        }
+        }       
         else
         {
             playerAnim.SetBool("Left Bool", false);
-
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            playerAnim.SetBool("Right Bool", true);
-            playerAnim.SetFloat("Right Float", 1);
+            GetComponent<Animator>().Play("right");
             transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
         }
         else
         {
             playerAnim.SetBool("Right Bool", false);
-
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            shotDelay += Time.deltaTime;
+            if (gameObject.transform.position.x < 0)
+            {
+                playerAnim.SetBool("Left Bool", true);
+                if (shotDelay > shotCool)
+                {
+                    Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+                    shotDelay = 0;
+                }
+            }            
+            if (gameObject.transform.position.x >= 0)
+            {
+                playerAnim.SetBool("Right Bool", true);
+                if (shotDelay > shotCool)
+                {
+                    Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+                    shotDelay = 0;
+                }                
+            }            
+        }
+        if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Animator>().Play("right");
+            if (shotDelay > shotCool)
+            {
+                Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+                shotDelay = 0;
+            }
+        }
+        if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Animator>().Play("left");
+            if (shotDelay > shotCool)
+            {
+                Instantiate(bullet, firePos.transform.position, firePos.transform.rotation);
+                shotDelay = 0;
+            }
+        }
+        if (transform.position.x > limtX)
+        {
+            transform.position = new Vector3(limtX, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -limtX)
+        {
+            transform.position = new Vector3(-limtX, transform.position.y, transform.position.z);
         }
     }
 }
