81ee94a0049907ae8eca14491f6f537484264615
910527ksj <910527ksj@gmail.com>
2018-10-18 11:02:30 +0900

    개인 연습
    개인 연습

---
+++ PangPang/Assets/Script/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float playerSpeed;
+    public Animator playerAnim;
+
+    void Start ()
+    {
+		
+	}
+	
+
+	
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerAnim.SetBool("Left Bool",true);
+            playerAnim.SetFloat("Left Float", 1);
+            transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            playerAnim.SetBool("Left Bool", false);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerAnim.SetBool("Right Bool", true);
+            playerAnim.SetFloat("Right Float", 1);
+            transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            playerAnim.SetBool("Right Bool", false);
+
+        }
+    }
+}
